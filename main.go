bfd15db0a2111b43571b0b3588dd88e63320a40b
andrews326 <andreamose988@gmail.com>
2024-06-24 14:57:06 +0530

    changes

--- main.go
+++ main.go
@@ -2,22 +2,25 @@ package main
 
 import (
 	"fmt"
-	"kerala/witchcraft/routes"
+	// "kerala/witchcraft/routes"
 	"log"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	err := routes.Init()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	err := routes.Init()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", HelloServer)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello World")
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 }
