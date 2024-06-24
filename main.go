10b8ed3d3259719ab1f82318d184fa4499a61d43
andrews326 <andreamose988@gmail.com>
2024-06-24 17:09:42 +0530

    api checking

--- main.go
+++ main.go
@@ -1,28 +1,54 @@
-package handler
+// package main
+
+// import (
+// 	"kerala/witchcraft/routes"
+// 	"log"
+// )
+
+// func main() {
+// 	err := routes.Init()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+//
+//}
+
+package main
 
 import (
+	"context"
 	"kerala/witchcraft/routes"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gofiber/adaptor/v2"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Create a global Fiber app instance
 var app *fiber.App
+var client *mongo.Client
 
 func init() {
-	// Initialize the Fiber app
+	// Initialize MongoDB client
+	var err error
+	clientOptions := options.Client().ApplyURI(os.Getenv("MONGODB_URI"))
+	client, err = mongo.Connect(context.Background(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Initialize Fiber app
 	app = fiber.New()
 
 	// Initialize routes
-	routes.RouteGroup(app)
+	routes.RouteGroup(app, client)
 }
 
 // Handler is the entry point for Vercel
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Use the Fiber adaptor to handle the request
 	adaptor.FiberApp(app)(w, r)
 }
 
--- routes/routes.go
+++ routes/routes.go
@@ -1,27 +1,39 @@
 package routes
 
 import (
-	"kerala/witchcraft/database"
 	_ "kerala/witchcraft/docs"
 	"kerala/witchcraft/handler"
 
 	"github.com/gofiber/fiber/v2"
-	fiberSwagger "github.com/swaggo/fiber-swagger"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func RouteGroup(app *fiber.App) {
-	group := app.Group("/api/v1/witchcraft")
+func RouteGroup(app *fiber.App, client *mongo.Client) {
+	h := handler.NewHandler(client)
+	app.Post("/create", h.CreateNewWitchcraft)
 
-	db, err := database.MongoConnection()
-	if err != nil {
-		panic(err) // For simplicity, panic on error
-	}
-	h := handler.NewHandler(db)
-
-	app.Get("/swagger/*", fiberSwagger.WrapHandler)
-	group.Post("/", h.CreateNewWitchcraft)
-	group.Put("/:id", h.UpdateWitchcraft)
-	group.Get("/", h.GetAllWitchcrafts)
-	group.Get("/:id", h.GetWitchcraftById)
-	group.Delete("/:id", h.DeleteWitchcraftById)
 }
+
+// func Init() error {
+// 	app := fiber.New()
+// 	r := RouteGroup(app)
+// 	db, err := database.MongoConnection()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	handler := handler.NewHandler(db)
+
+// 	app.Get("/swagger/*", fiberSwagger.WrapHandler)
+// 	r.Post("/", handler.CreateNewWitchcraft)
+// 	r.Put("/:id", handler.UpdateWitchcraft)
+// 	r.Get("/", handler.GetAllWitchcrafts)
+// 	r.Get("/:id", handler.GetWitchcraftById)
+// 	r.Delete("/:id", handler.DeleteWitchcraftById)
+
+// 	err = app.Listen(":8080")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+
+// }
