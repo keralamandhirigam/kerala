90bc93040e51e09866d31a9f169a5cca89599df7
andrews326 <andreamose988@gmail.com>
2024-06-24 10:11:15 +0530

    added vercel.json file

--- database/database.go
+++ database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"context"
+	"fmt"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -10,7 +11,7 @@ import (
 
 func MongoConnection() (*mongo.Client, error) {
 	opts := options.Client()
-	opts.ApplyURI("mongodb://localhost:27017")
+	opts.ApplyURI("mongodb+srv://andreamose988:E6xYOfUn0erPPQYa@cluster0.4wntr2x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 	client, err := mongo.NewClient(opts)
 	if err != nil {
@@ -20,6 +21,8 @@ func MongoConnection() (*mongo.Client, error) {
 	err = client.Connect(context.TODO())
 	if err != nil {
 		return nil, err
+	} else {
+		fmt.Println("connection success")
 	}
 
 	err = client.Ping(context.TODO(), readpref.PrimaryPreferred())
